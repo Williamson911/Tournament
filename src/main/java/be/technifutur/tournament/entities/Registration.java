60fb85ee48d4f5b10715844520b598ec2a591134
Will <william.williamson.91@gmail.com>
2026-05-04 15:14:35 +0200

    Rename Character, persistence and RegistrationDAO

--- src/main/java/be/technifutur/tournament/entities/Registration.java
+++ src/main/java/be/technifutur/tournament/entities/Registration.java
@@ -28,7 +28,7 @@ public class Registration {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "character_id", nullable = false)
-    private CharacterSF character;
+    private Character character;
 
     @Column(name = "registered_at", nullable = false, updatable = false)
     private LocalDateTime registeredAt;
