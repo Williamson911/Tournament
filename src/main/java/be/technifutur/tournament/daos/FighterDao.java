cdd20aba99101ce6724723770d82a303471de125
Will <william.williamson.91@gmail.com>
2026-05-04 16:55:41 +0200

    try fix 500

--- src/main/java/be/technifutur/tournament/daos/FighterDao.java
+++ src/main/java/be/technifutur/tournament/daos/FighterDao.java
@@ -1,37 +1,17 @@
 package be.technifutur.tournament.daos;
 
 import be.technifutur.tournament.entities.Fighter;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 
-@ApplicationScoped
-public class FighterDao extends CrudDao<Fighter,Integer>{
+public class FighterDao extends CrudDao<Fighter, Long> {
 
-//    public Optional<Character> findByEmailOrUsername(String login){
-//        try(EntityManager em = emf.createEntityManager()){
-//            Character user = em.createQuery("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :login OR u.username = :login",User.class)
-//                    .setParameter("login",login)
-//                    .getSingleResult();
-//
-//            return Optional.ofNullable(user);
-//        }
-//    }
-
-//    public boolean existsByEmail(@NotBlank String email) {
-//
-//        try(EntityManager em = emf.createEntityManager()){
-//            return em.createQuery("SELECT COUNT(u) FROM User u WHERE u.email = :email",Long.class)
-//                    .setParameter("email",email)
-//                    .getSingleResult() > 0;
-//        }
-//    }
-
-    public Fighter getFighterByName(String name){
-        try(EntityManager em = emf.createEntityManager()){
-            Fighter fighter =  em.createQuery("SELECT f FROM Fighter f WHERE f.name= :name",Fighter.class)
-                    .setParameter("name",name)
-                    .getSingleResult();
-            return fighter;
+    public Fighter getFighterByName(String name) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("SELECT f FROM Fighter f WHERE f.name = :name", Fighter.class)
+                    .setParameter("name", name)
+                    .getResultStream()
+                    .findFirst()
+                    .orElse(null);
         }
     }
 
