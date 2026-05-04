c06049134090ba409376af8a0efcb72b210a7883
Will <william.williamson.91@gmail.com>
2026-05-04 16:13:07 +0200

    fixed col names

--- src/main/java/be/technifutur/tournament/entities/MatchResult.java
+++ src/main/java/be/technifutur/tournament/entities/MatchResult.java
@@ -17,11 +17,11 @@ public class MatchResult {
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "match_id", nullable = false, unique = true)
+    @JoinColumn(name = "id_match", nullable = false, unique = true)
     private Match match;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "winner_id", nullable = false)
+    @JoinColumn(name = "id_winner", nullable = false)
     private Player winner;
 
     @Column(name = "player1_score", nullable = false)
@@ -30,7 +30,7 @@ public class MatchResult {
     @Column(name = "player2_score", nullable = false)
     private Integer player2Score;
 
-    @Column(name = "rounds_played", nullable = false)
+    @Column(name = "nb_rounds", nullable = false)
     private Integer roundsPlayed;
 
     @Column(name = "finish_type")
