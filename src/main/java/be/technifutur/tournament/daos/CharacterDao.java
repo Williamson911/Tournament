bfa9fed747ac456c403d4e14b8a6526b0922264a
ramzan <ramzan2000@hotmail.com>
2026-05-04 15:36:39 +0200

    test

---
+++ src/main/java/be/technifutur/tournament/daos/CharacterDao.java
@@ -0,0 +1,36 @@
+package be.technifutur.tournament.daos;
+
+import be.technifutur.tournament.entities.Fighter;
+import jakarta.persistence.EntityManager;
+
+public class CharacterDao extends CrudDao<Fighter,Integer>{
+
+//    public Optional<Character> findByEmailOrUsername(String login){
+//        try(EntityManager em = emf.createEntityManager()){
+//            Character user = em.createQuery("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :login OR u.username = :login",User.class)
+//                    .setParameter("login",login)
+//                    .getSingleResult();
+//
+//            return Optional.ofNullable(user);
+//        }
+//    }
+
+//    public boolean existsByEmail(@NotBlank String email) {
+//
+//        try(EntityManager em = emf.createEntityManager()){
+//            return em.createQuery("SELECT COUNT(u) FROM User u WHERE u.email = :email",Long.class)
+//                    .setParameter("email",email)
+//                    .getSingleResult() > 0;
+//        }
+//    }
+
+    public Fighter getFighterByName(String name){
+        try(EntityManager em = emf.createEntityManager()){
+            Fighter fighter =  em.createQuery("SELECT f FROM Fighter f WHERE f.name= :name",Fighter.class)
+                    .setParameter("name",name)
+                    .getSingleResult();
+            return fighter;
+        }
+    }
+
+}
