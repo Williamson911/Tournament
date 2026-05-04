57bbac9ac4a996a547cfc56a652daf9f5a2f0370
ramzan <ramzan2000@hotmail.com>
2026-05-04 11:56:17 +0200

    versionningin automatique configurer pour chaque execution de l'app

--- src/main/java/be/technifutur/tournament/HelloApplication.java
+++ src/main/java/be/technifutur/tournament/HelloApplication.java
@@ -1,9 +1,15 @@
 package be.technifutur.tournament;
 
+import be.technifutur.tournament.utils.AppInfo;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
 
+    public HelloApplication(){
+        AppInfo appInfo = new AppInfo();
+        appInfo.getCurrentVersion();
+        System.out.println(AppInfo.staticVersion);
+    }
 }
