45fcd1eea0e576625ba13bc8899320b2f1e328e9
ramzan <ramzan2000@hotmail.com>
2026-05-06 12:39:21 +0200

    2026.05.06.0011 1.fighterDao fighterResources

--- src/main/java/be/technifutur/tournament/daos/FighterDao.java
+++ src/main/java/be/technifutur/tournament/daos/FighterDao.java
@@ -8,34 +8,6 @@ import java.util.List;
 
 @ApplicationScoped
 public class FighterDao extends CrudDao<Fighter,Integer>{
-
-//    public Optional<Character> findByEmailOrUsername(String login){
-//        try(EntityManager em = emf.createEntityManager()){
-//            Character user = em.createQuery("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :login OR u.username = :login",User.class)
-//                    .setParameter("login",login)
-//                    .getSingleResult();
-//
-//            return Optional.ofNullable(user);
-//        }
-//    }
-
-//    public boolean existsByEmail(@NotBlank String email) {
-//
-//        try(EntityManager em = emf.createEntityManager()){
-//            return em.createQuery("SELECT COUNT(u) FROM User u WHERE u.email = :email",Long.class)
-//                    .setParameter("email",email)
-//                    .getSingleResult() > 0;
-//        }
-//    }
-
-//    public List<Fighter> getAllFighters(){
-//        try(EntityManager em = emf.createEntityManager()){
-//            return em.createQuery("SELECT f FROM Fighter f",Fighter.class).getResultList();
-//        }
-//    }
-
-
-
     public Fighter getFighterByName(String name){
         try(EntityManager em = emf.createEntityManager()){
             Fighter fighter =  em.createQuery("SELECT f FROM Fighter f WHERE f.name ILIKE :name",Fighter.class)
@@ -44,5 +16,4 @@ public class FighterDao extends CrudDao<Fighter,Integer>{
             return fighter;
         }
     }
-
 }
